70b4a236627ae25b6a63d3c609d7dd2778a82db6
Iaro <iaroslav.melekhov@aalto.fi>
2021-01-20 21:25:43 +0200

    Pipeline added

--- relposenet/augmentations.py
+++ relposenet/augmentations.py
@@ -6,12 +6,6 @@ def get_imagenet_mean_std():
     return mean, std
 
 
-def net_preprocessing():
-    mean, std = get_imagenet_mean_std()
-    transform = transforms.Normalize(mean, std)
-    return transform
-
-
 def get_augmentations():
     train_aug = train_augmentations()
     val_aug = eval_augmentations()
@@ -19,21 +13,25 @@ def get_augmentations():
 
 
 def train_augmentations():
+    mean, std = get_imagenet_mean_std()
     transform = transforms.Compose([transforms.RandomCrop(size=224),
                                     transforms.RandomChoice([
                                         transforms.ColorJitter(brightness=0.4,
                                                                contrast=0.4,
                                                                saturation=0.4),
                                         transforms.RandomGrayscale()]),
-                                    transforms.ToTensor()
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean, std)
                                     ])
 
     return transform
 
 
 def eval_augmentations():
+    mean, std = get_imagenet_mean_std()
     transform = transforms.Compose([transforms.CenterCrop(size=224),
-                                    transforms.ToTensor()
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean, std)
                                     ])
 
     return transform
--- relposenet/pipeline.py
+++ relposenet/pipeline.py
@@ -3,20 +3,25 @@ from os import path as osp
 import time
 from tqdm import tqdm
 import torch
+from tensorboardX import SummaryWriter
 from relposenet.model import RelPoseNet
 from relposenet.dataset import SevenScenesRelPoseDataset
 from relposenet.augmentations import get_augmentations
+from relposenet.criterion import RelPoseCriterion
+from relposenet.utils import cycle
 
 
 class Pipeline(object):
     def __init__(self, cfg):
         self.cfg = cfg
         cfg_model = self.cfg.model_params
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # initialize dataloaders
         self.train_loader, self.val_loader = self._init_dataloaders()
+        self.train_loader_iterator = iter(cycle(self.train_loader))
 
-        self.model = RelPoseNet(cfg_model)
+        self.model = RelPoseNet(cfg_model).to(self.device)
 
         # Optimizer
         self.optimizer = torch.optim.Adam(self.model.parameters(),
@@ -27,6 +32,12 @@ class Pipeline(object):
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
                                                          step_size=cfg_scheduler.lrate_decay_steps,
                                                          gamma=cfg_scheduler.lrate_decay_factor)
+
+        # Criterion
+        self.criterion = RelPoseCriterion(self.cfg.train_params.alpha).to(self.device)
+
+        # create writer (logger)
+        self.writer = SummaryWriter(self.cfg.output_params.logger_dir)
 
         self.start_step = 0
         self.val_total_loss = 1e6
@@ -40,13 +51,9 @@ class Pipeline(object):
         # get image augmentations
         train_augs, val_augs = get_augmentations()
 
-        train_dataset = SevenScenesRelPoseDataset(cfg=self.cfg,
-                                                  split='train',
-                                                  transforms=train_augs)
+        train_dataset = SevenScenesRelPoseDataset(cfg=self.cfg, split='train', transforms=train_augs)
 
-        val_dataset = SevenScenesRelPoseDataset(cfg=self.cfg,
-                                                split='val',
-                                                transforms=val_augs)
+        val_dataset = SevenScenesRelPoseDataset(cfg=self.cfg, split='val', transforms=val_augs)
 
         train_loader = torch.utils.data.DataLoader(train_dataset,
                                                    batch_size=cfg_train.bs,
@@ -54,6 +61,7 @@ class Pipeline(object):
                                                    pin_memory=True,
                                                    num_workers=cfg_train.n_workers,
                                                    drop_last=True)
+
         val_loader = torch.utils.data.DataLoader(val_dataset,
                                                  batch_size=cfg_train.bs,
                                                  shuffle=False,
@@ -62,13 +70,18 @@ class Pipeline(object):
                                                  drop_last=True)
         return train_loader, val_loader
 
+    def _predict_cam_pose(self, mini_batch):
+        q_est, t_est = self.model.forward(mini_batch['img1'].to(self.device),
+                                          mini_batch['img2'].to(self.device))
+        return q_est, t_est
+
     def _save_model(self, step, loss_val, best_val=False):
         if not osp.exists(self.cfg.output_params.snapshot_dir):
             os.makedirs(self.cfg.output_params.snapshot_dir)
 
         fname_out = 'best_val.pth' if best_val else 'snapshot{:06d}.pth'.format(step)
         save_path = osp.join(self.cfg.output_params.snapshot_dir, fname_out)
-        model_state = self.model.get_model().state_dict()
+        model_state = self.model.state_dict()
         torch.save({'step': step,
                     'state_dict': model_state,
                     'optimizer': self.optimizer.state_dict(),
@@ -79,7 +92,7 @@ class Pipeline(object):
 
     def _load_model(self, snapshot):
         data_dict = torch.load(snapshot)
-        self.model.get_model().load_state_dict(data_dict['state_dict'])
+        self.model.load_state_dict(data_dict['state_dict'])
         self.optimizer.load_state_dict(data_dict['optimizer'])
         self.scheduler.load_state_dict(data_dict['scheduler'])
         self.start_step = data_dict['step']
@@ -88,38 +101,54 @@ class Pipeline(object):
 
     def _train_batch(self):
         train_sample = next(self.train_loader_iterator)
-        loc_desc_anc, loc_desc_pos, kpts_crop_ids = self._get_local_descs(train_sample)
+        q_est, t_est = self._predict_cam_pose(train_sample)
+
         self.optimizer.zero_grad()
+
         # compute loss
-        loss, ap = self.criterion(loc_desc_anc, loc_desc_pos, kpts_crop_ids)
+        loss, t_loss_val, q_loss_val = self.criterion(train_sample['q_gt'].to(self.device),
+                                                      train_sample['t_gt'].to(self.device),
+                                                      q_est,
+                                                      t_est)
         loss.backward()
+
         # update the optimizer
         self.optimizer.step()
+
         # update the scheduler
         self.scheduler.step()
-        return loss.item(), ap.item()
+        return loss.item(), t_loss_val, q_loss_val
 
     def _validate(self):
-        self.model.get_model().eval()
-        loss_value = 0.
-        ap_value = 0.
+        self.model.eval()
+        loss_total, t_loss_total, q_loss_total = 0., 0., 0.
+
         with torch.no_grad():
             for val_sample in tqdm(self.val_loader):
-                loc_desc_anc, loc_desc_pos, kpts_crop_ids = self._get_local_descs(val_sample)
+                q_est, t_est = self._predict_cam_pose(val_sample)
                 # compute loss
-                loss, ap = self.criterion(loc_desc_anc, loc_desc_pos, kpts_crop_ids)
-                loss_value += loss.item()
-                ap_value += ap.item()
+                loss, t_loss_val, q_loss_val = self.criterion(val_sample['q_gt'].to(self.device),
+                                                              val_sample['t_gt'].to(self.device),
+                                                              q_est,
+                                                              t_est)
+                loss_total += loss.item()
+                t_loss_total += t_loss_val
+                q_loss_total += q_loss_val
 
-        self.model.get_model().train()
-        return loss_value / len(self.val_loader), ap_value / len(self.val_loader)
+        avg_total_loss = loss_total / len(self.val_loader)
+        avg_t_loss = t_loss_total / len(self.val_loader)
+        avg_q_loss = q_loss_total / len(self.val_loader)
+
+        self.model.train()
+
+        return avg_total_loss, avg_t_loss, avg_q_loss
 
     def run(self):
         print('Start training', self.start_step)
         train_start_time = time.time()
         train_log_iter_time = time.time()
         for step in range(self.start_step + 1, self.start_step + self.cfg.train_params.n_train_iters):
-            train_loss_batch, ap_batch = self._train_batch()
+            train_loss_batch, _, _ = self._train_batch()
 
             if step % self.cfg.output_params.log_scalar_interval == 0 and step > 0:
                 self.writer.add_scalar('Train_total_loss_batch', train_loss_batch, step)
@@ -127,19 +156,20 @@ class Pipeline(object):
                       f'{(time.time() - train_log_iter_time) / 60.}')
                 train_log_iter_time = time.time()
                 print(f'Step {step} out of {self.cfg.train_params.n_train_iters} is done. Train loss (per batch): '
-                      f'{train_loss_batch}. AP: {ap_batch}')
+                      f'{train_loss_batch}.')
 
             if step % self.cfg.output_params.validate_interval == 0 and step > 0:
                 val_time = time.time()
                 best_val = False
-                val_loss, ap_val = self._validate()
-                self.writer.add_scalar('Val_total_loss', val_loss, step)
-                self.writer.add_scalar('Val_total_AP', ap_val, step)
-                if val_loss < self.val_total_loss:
-                    self.val_total_loss = val_loss
+                val_total_loss, val_t_loss, val_q_loss = self._validate()
+                self.writer.add_scalar('Val_total_loss', val_total_loss, step)
+                self.writer.add_scalar('Val_t_loss', val_t_loss, step)
+                self.writer.add_scalar('Val_q_loss', val_q_loss, step)
+                if val_total_loss < self.val_total_loss:
+                    self.val_total_loss = val_total_loss
                     best_val = True
-                self._save_model(step, val_loss, best_val=best_val)
-                print(f'Validation loss: {val_loss}, ap: {ap_val}')
+                self._save_model(step, val_total_loss, best_val=best_val)
+                print(f'Validation loss: {val_total_loss}, t_loss: {val_t_loss}, q_loss: {val_q_loss}')
                 print(f'Elapsed time [min] for validation: {(time.time() - val_time) / 60.}')
                 train_log_iter_time = time.time()
 
